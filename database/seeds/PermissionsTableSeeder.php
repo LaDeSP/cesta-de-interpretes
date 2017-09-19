ec242b27238d6170c462b5b62f17f7775b3f7aeb
Diego Tsuyoshi <dttsuyoshi@gmail.com>
2017-09-19 08:01:39 -0400

    Adicionadas algumas permissões de Request 'solicitação'

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -9,8 +9,45 @@ class PermissionsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        //
+
+     public function run()
+     {
+        /**
+         * Requests oermissions
+         *
+         */
+        DB::table('permissions')->insert([[
+            // Partners
+            'name' => 'create-request',
+            'display_name' => 'Create request',
+            'description' => 'creates a request',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ],  
+            // Admins, Partners 
+            [
+            'name' => 'close-request',
+            'display_name' => 'Close request',
+            'description' => 'closes a request',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ],
+            // Admins, Partners
+            [
+            'name' => 'edit-request',
+            'display_name' => 'Edit request',
+            'description' => 'edits a request',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ], 
+            // Interpreter
+            [
+            'name' => 'answer-request',
+            'display_name' => 'Answer request',
+            'description' => 'answers to a request',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ], 
+        ]);
     }
 }
